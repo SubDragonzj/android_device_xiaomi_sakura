5a828d6c2129717cb0a186ca79cbf341e086c66c
maxwen <max.weninger@gmail.com>
2016-09-29 00:41:15 +0200

    sakura: add mediaextractor seccomp policy
    Change-Id: I04d709f88db4258658991cbeacccd3b399c662f8

--- device.mk
+++ device.mk
@@ -255,7 +255,8 @@ PRODUCT_COPY_FILES += \
 
 # Seccomp policy
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/seccomp_policy/mediacodec.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediacodec.policy
+    $(LOCAL_PATH)/seccomp_policy/mediacodec.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediacodec.policy \
+    $(LOCAL_PATH)/seccomp_policy/mediaextractor-seccomp.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediaextractor.policy
 
 # FM
 PRODUCT_PACKAGES += \
