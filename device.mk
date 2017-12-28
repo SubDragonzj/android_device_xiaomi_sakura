be1da3f6f58d06c2ef43c621f4963573b8df65dc
Mikhail Naganov <mnaganov@google.com>
2017-12-28 13:29:05 -0800

    sakura: Switch to Soundtrigger 2.1 implementation
    Bug: 68823037
    Test: "ok google" from locked screen
    Change-Id: I256bae45bc3c68febe5d7bb4bdbd559d752b7d96

--- device.mk
+++ device.mk
@@ -98,8 +98,8 @@ PRODUCT_PACKAGES += \
     android.hardware.audio@2.0-service \
     android.hardware.audio.effect@2.0-impl \
     android.hardware.audio.effect@2.0-service \
-    android.hardware.soundtrigger@2.0-impl \
-    android.hardware.soundtrigger@2.0-service
+    android.hardware.soundtrigger@2.1-impl \
+    android.hardware.soundtrigger@2.1-service
 
 PRODUCT_PROPERTY_OVERRIDES += \
     af.fast_track_multiplier=2 \
