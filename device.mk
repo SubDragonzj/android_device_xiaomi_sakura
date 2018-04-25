500e871b32a387f495f2d8740304adebb23dcde9
subdragonzj <zj502407679@gmail.com>
2018-04-25 21:16:07 +0800

    rosy: build vndk-sp

--- device.mk
+++ device.mk
@@ -441,6 +441,10 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.vulkan.level-0.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.vulkan.level-0.xml \
     frameworks/native/data/etc/android.hardware.vulkan.version-1_0_3.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.vulkan.version-1_0_3.xml
 
+# VNDK-SP
+PRODUCT_PACKAGES += \
+    vndk-sp
+
 # WIFI
 PRODUCT_PACKAGES += \
     hostapd \
