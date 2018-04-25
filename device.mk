6408f933220beb3733e4d0ca1681e8fab3373adf
subdragonzj <zj502407679@gmail.com>
2018-04-25 21:14:23 +0800

    rosy: use custom biometrics

--- device.mk
+++ device.mk
@@ -166,6 +166,10 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_PACKAGES += \
     android.hardware.drm@1.0-impl
 
+# Fingerprint
+PRODUCT_PACKAGES += \
+    android.hardware.biometrics.fingerprint@2.0-service-custom
+
 # FM
 PRODUCT_PACKAGES += \
     FMRadio \
