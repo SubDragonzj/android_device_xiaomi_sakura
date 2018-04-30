e0f882b6f4d9b2c5321317f52c8ff02241a71650
subdragonzj <zj502407679@gmail.com>
2018-04-30 22:05:27 +0800

    rosy: update fingerprint(8.4.12)

--- mokee.mk
+++ mokee.mk
@@ -36,8 +36,8 @@ PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 TARGET_VENDOR_PRODUCT_NAME := rosy
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    BUILD_FINGERPRINT=Xiaomi/rosy/rosy:7.1.2/N2G47H/V9.5.1.0.NDACNFA:user/release-keys \
-    PRIVATE_BUILD_DESC="rosy-user 7.1.2 N2G47H V9.5.1.0.NDACNFA release-keys"
+    BUILD_FINGERPRINT=Xiaomi/rosy/rosy:7.1.2/N2G47H/8.4.12:user/release-keys \
+    PRIVATE_BUILD_DESC="rosy-user 7.1.2 N2G47H 8.4.12 release-keys"
 
 PRODUCT_SYSTEM_PROPERTY_BLACKLIST += \
     ro.product.model
