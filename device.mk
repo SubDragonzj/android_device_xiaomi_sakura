404f5156c28dac3e27e1a21e94a99f8075d6d0ca
subdragonzj <zj502407679@gmail.com>
2018-06-15 23:04:48 +0800

    rosy: Remove non-existing HAL services

--- device.mk
+++ device.mk
@@ -92,9 +92,7 @@ PRODUCT_PACKAGES += \
     android.hardware.audio@2.0-impl \
     android.hardware.audio@2.0-service \
     android.hardware.audio.effect@2.0-impl \
-    android.hardware.audio.effect@2.0-service \
-    android.hardware.soundtrigger@2.0-impl \
-    android.hardware.soundtrigger@2.0-service
+    android.hardware.soundtrigger@2.0-impl
 
 PRODUCT_PROPERTY_OVERRIDES += \
     vendor.voice.playback.conc.disabled=true \
