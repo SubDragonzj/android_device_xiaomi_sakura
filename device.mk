6f7c71b3cb22d83854da441182ce54371bf5f9fd
subdragonzj <zj502407679@gmail.com>
2018-06-15 23:25:04 +0800

    rosy: no need to build display.config packages

--- device.mk
+++ device.mk
@@ -198,9 +198,7 @@ PRODUCT_PACKAGES += \
     android.hardware.graphics.composer@2.1-service \
     android.hardware.graphics.mapper@2.0-impl \
     android.hardware.memtrack@1.0-impl \
-    android.hardware.memtrack@1.0-service \
-    vendor.display.config@1.0 \
-    vendor.display.config@1.0_vendor
+    android.hardware.memtrack@1.0-service
 
 PRODUCT_PROPERTY_OVERRIDES += \
     debug.gralloc.enable_fb_ubwc=1 \
