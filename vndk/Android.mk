2c49e3ff01274d1758f3ea064978a369fb309934
TheScarastic <warabhishek@gmail.com>
2018-08-15 13:07:06 +0530

    sakura: Install vndk libs to vndk-sp-28
    Change-Id: I186ba6c286900ae68fca1ac24dc400db21b416bb

--- vndk/Android.mk
+++ vndk/Android.mk
@@ -1,9 +1,7 @@
 VNDK_SP_LIBRARIES := \
     android.hardware.graphics.allocator@2.0 \
     android.hardware.graphics.common@1.0 \
-    android.hardware.graphics.common@1.1 \
     android.hardware.graphics.mapper@2.0 \
-    android.hardware.graphics.mapper@2.1 \
     android.hardware.renderscript@1.0 \
     android.hidl.memory@1.0 \
     libRSCpuRef \
@@ -26,11 +24,8 @@ VNDK_SP_LIBRARIES := \
     liblzma \
     libpng \
     libunwind \
-    libunwindstack \
     libutils \
-    libz \
-    libutilscallstack \
-    libdexfile
+    libz
 
 EXTRA_VENDOR_LIBRARIES := \
     android.hidl.base@1.0 \
@@ -38,18 +33,10 @@ EXTRA_VENDOR_LIBRARIES := \
     vendor.display.color@1.0 \
     vendor.display.config@1.0
 
-INSTALL_IN_HW_SUBDIR := android.hidl.memory@1.0-impl
 
 #-------------------------------------------------------------------------------
 # VNDK Modules
 #-------------------------------------------------------------------------------
-
-ifdef PLATFORM_VNDK_VERSION
-VNDK_SP_DIR := vndk-sp-$(PLATFORM_VNDK_VERSION)
-else
-VNDK_SP_DIR := vndk-sp
-endif
-
 LOCAL_PATH := $(call my-dir)
 
 define define-vndk-lib
@@ -62,7 +49,7 @@ LOCAL_MULTILIB := first
 LOCAL_MODULE_TAGS := optional
 LOCAL_INSTALLED_MODULE_STEM := $1.so
 LOCAL_MODULE_SUFFIX := .so
-LOCAL_MODULE_RELATIVE_PATH := $3$(if $(filter $1,$(INSTALL_IN_HW_SUBDIR)),/hw)
+LOCAL_MODULE_RELATIVE_PATH := $3
 LOCAL_VENDOR_MODULE := $4
 include $$(BUILD_PREBUILT)
 
@@ -77,7 +64,7 @@ LOCAL_MULTILIB := 32
 LOCAL_MODULE_TAGS := optional
 LOCAL_INSTALLED_MODULE_STEM := $1.so
 LOCAL_MODULE_SUFFIX := .so
-LOCAL_MODULE_RELATIVE_PATH := $3$(if $(filter $1,$(INSTALL_IN_HW_SUBDIR)),/hw)
+LOCAL_MODULE_RELATIVE_PATH := $3
 LOCAL_VENDOR_MODULE := $4
 include $$(BUILD_PREBUILT)
 endif  # TARGET_TRANSLATE_2ND_ARCH is not true
@@ -85,13 +72,12 @@ endif  # TARGET_2ND_ARCH is not empty
 endef
 
 $(foreach lib,$(VNDK_SP_LIBRARIES),\
-    $(eval $(call define-vndk-lib,$(lib),vndk-sp-gen,$(VNDK_SP_DIR),)))
+    $(eval $(call define-vndk-lib,$(lib),vndk-sp-gen,vndk-sp-28,)))
 $(foreach lib,$(VNDK_SP_EXT_LIBRARIES),\
-    $(eval $(call define-vndk-lib,$(lib),vndk-sp-ext-gen,$(VNDK_SP_DIR),true)))
+    $(eval $(call define-vndk-lib,$(lib),vndk-sp-ext-gen,vndk-sp,true)))
 $(foreach lib,$(EXTRA_VENDOR_LIBRARIES),\
     $(eval $(call define-vndk-lib,$(lib),vndk-ext-gen,,true)))
 
-VNDK_SP_DIR :=
 
 #-------------------------------------------------------------------------------
 # Phony Package
@@ -106,7 +92,3 @@ LOCAL_REQUIRED_MODULES := \
     $(addsuffix .vndk-ext-gen,$(EXTRA_VENDOR_LIBRARIES))
 include $(BUILD_PHONY_PACKAGE)
 
-VNDK_SP_LIBRARIES :=
-VNDK_SP_EXT_LIBRARIES :=
-EXTRA_VENDOR_LIBRARIES :=
-INSTALL_IN_HW_SUBDIR :=
