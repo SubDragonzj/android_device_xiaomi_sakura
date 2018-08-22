550746bdfa65250de3ab537ec36aadbcefe076ad
Yifan Hong <elsk@google.com>
2018-08-22 09:32:55 +0200

    sakura: Health: Uprevision to 2.0
    Test: adb logcat -e health
    Test: lshal --debug
    Change-Id: I48e3f18f79dd82d2320c384d12332bd49c14c4bc

--- device.mk
+++ device.mk
@@ -254,8 +254,8 @@ PRODUCT_PACKAGES += \
 
 # Healthd
 PRODUCT_PACKAGES += \
-    android.hardware.health@1.0-impl \
-    android.hardware.health@1.0-service
+    android.hardware.health@2.0-impl \
+    android.hardware.health@2.0-service
 
 # HIDL
 PRODUCT_PACKAGES += \
