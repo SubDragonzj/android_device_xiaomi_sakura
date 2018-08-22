dd0a68a4575071955d23fb29a689eca8afff3e5a
subdragonzj <zj502407679@gmail.com>
2018-08-22 21:39:18 +0800

    rosy: device.mk: add sensors.msm8953.so

--- device.mk
+++ device.mk
@@ -295,7 +295,8 @@ PRODUCT_PACKAGES += \
 # Sensors
 PRODUCT_PACKAGES += \
     android.hardware.sensors@1.0-impl \
-    android.hardware.sensors@1.0-service
+    android.hardware.sensors@1.0-service \
+    sensors.msm8953
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/sensors/hals.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/hals.conf \
