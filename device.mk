b1c433f5192419d5580fe3ddc6e38aa5ac6d8e5c
subdragonzj <zj502407679@gmail.com>
2018-10-14 02:25:59 +0800

    rosy: add maintainer
    Change-Id: Ia47190e01d904b39302cb592d871609cc98b8ec1

--- device.mk
+++ device.mk
@@ -533,3 +533,7 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/wifi/WCNSS_cfg.dat:$(TARGET_COPY_OUT_VENDOR)/etc/firmware/wlan/prima/WCNSS_cfg.dat \
     $(LOCAL_PATH)/wifi/WCNSS_qcom_cfg.ini:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/WCNSS_qcom_cfg.ini
+    
+# Maintainer
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.mk.maintainer=subdragonzj
