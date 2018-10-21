6b994056a1bdb6ae4f7c9bd10c512c41af833e34
subdragonzj <zj502407679@gmail.com>
2018-10-21 21:35:07 +0800

    rosy: update configs
    * msm8953-common
    
    Change-Id: I0040480164ede8e3736b485cb1b996414de0cf31

--- device.mk
+++ device.mk
@@ -413,7 +413,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/media_codecs.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs.xml \
     $(LOCAL_PATH)/configs/media_codecs_performance.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_performance.xml \
-    $(LOCAL_PATH)/configs/media_profiles_8953_v1.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_profiles_8953_v1.xml
+    $(LOCAL_PATH)/configs/media_profiles_V1_0.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_profiles_V1_0.xml
 
 # Whitelisted app
 PRODUCT_COPY_FILES += \
