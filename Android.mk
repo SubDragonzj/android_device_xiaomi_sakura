065262da0ab77ffb332938945fabc7783e4f728d
subdragonzj <zj502407679@gmail.com>
2018-10-23 00:19:33 +0800

    rosy: Android:mk: IMS link to vendor
    Change-Id: Id6237140d957548c754598788bd628338743dba3

--- Android.mk
+++ Android.mk
@@ -62,12 +62,13 @@ $(LOCAL_BUILT_MODULE):
 	$(hide) touch $@
 
 IMS_LIBS := libimscamera_jni.so libimsmedia_jni.so
-IMS_SYMLINKS := $(addprefix $(TARGET_OUT)/priv-app/ims/lib/arm64/,$(notdir $(IMS_LIBS)))
+
+IMS_SYMLINKS := $(addprefix $(TARGET_OUT_VENDOR)/app/ims/lib/arm64/,$(notdir $(IMS_LIBS)))
 $(IMS_SYMLINKS): $(LOCAL_INSTALLED_MODULE)
 	@echo "IMS lib link: $@"
 	@mkdir -p $(dir $@)
 	@rm -rf $@
-	$(hide) ln -sf /system/lib64/$(notdir $@) $@
+	$(hide) ln -sf /vendor/lib64/$(notdir $@) $@
 
 ALL_DEFAULT_INSTALLED_MODULES += $(IMS_SYMLINKS)
 
