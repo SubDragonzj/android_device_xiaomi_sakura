f83c67ff62e89420342886b03f56c890c1d64101
subdragonzj <zj502407679@gmail.com>
2018-12-09 14:06:12 +0800

    Revert "sakura: seccomp: add mediaextractor.policy"
    This reverts commit d341a020b40acd3f2141d7d6735596fcf5b1e0a7.

--- device.mk
+++ device.mk
@@ -346,8 +346,7 @@ PRODUCT_COPY_FILES += \
 
 # Seccomp policy
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/seccomp_policy/mediacodec.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediacodec.policy \
-    $(LOCAL_PATH)/seccomp_policy/mediaextractor.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediaextractor.policy
+    $(LOCAL_PATH)/seccomp_policy/mediacodec.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediacodec.policy
 
 # Perf
 PRODUCT_PROPERTY_OVERRIDES += \
