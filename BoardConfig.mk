a225430db1781f6b40d3c8724a3d24c9310038c7
subdragonzj <zj502407679@gmail.com>
2019-01-04 18:44:21 +0800

    sakura: update Security patch level

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -235,4 +235,4 @@ PRODUCT_FULL_TREBLE_OVERRIDE := true
 include vendor/xiaomi/sakura/BoardConfigVendor.mk
 
 # Security patch level
-VENDOR_SECURITY_PATCH := 2018-01-01
+VENDOR_SECURITY_PATCH := 2018-10-05
