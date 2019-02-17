aff082266635214b9eb31d07cffa8c3fdca0d256
Artem Borisov <dedsa2002@gmail.com>
2019-02-17 23:08:33 +0530

    sakura: Add QC RIL service support
    * Remove android.hardware.radio.deprecated from manifest
    * Pin modified qcrild.rc
    
    Change-Id: I79e1242fe1de021905caddf5ed1085b2ec0c1146

--- device.mk
+++ device.mk
@@ -341,6 +341,7 @@ PRODUCT_PACKAGES += \
 
 # Radio
 PRODUCT_PACKAGES += \
+    android.hardware.radio@1.2 \
     android.hardware.radio.config@1.0 \
     android.hardware.secure_element@1.0 \
     libcnefeatureconfig \
