8cc7d5ec5150561667b980b369d2f20255cf7000
Nicolas Geoffray <ngeoffray@google.com>
2019-02-22 15:38:22 +0530

    sakura: Generate the apex image.
    Change-Id: Ib63bb91f20ae47715288cba32357b40527aa852d

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -19,6 +19,9 @@ LOCAL_PATH := device/xiaomi/sakura
 # ANT
 BOARD_ANT_WIRELESS_DEVICE := "qualcomm-hidl"
 
+# APEX image
+DEXPREOPT_GENERATE_APEX_IMAGE := true
+
 # Assert
 TARGET_BOARD_INFO_FILE := $(LOCAL_PATH)/board-info.txt
 TARGET_OTA_ASSERT_DEVICE := sakura
