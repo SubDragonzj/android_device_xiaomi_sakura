309c043dc84f2ed9c4c5cbbee8bb8551609dbe21
subdragonzj <zj502407679@gmail.com>
2019-02-22 20:49:26 +0800

    sakura: update product_launched to o_mr1
    Change-Id: I35d7bffc5f9209268c9a2ae1c78952b3b0f05a69

--- mk_sakura.mk
+++ mk_sakura.mk
@@ -19,7 +19,7 @@
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_n_mr1.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o_mr1.mk)
 
 # Inherit some common MoKee stuff
 $(call inherit-product, vendor/mk/config/common_full_phone.mk)
