416748586749b5583ce6b71506eb601d7ec2348b
subdragonzj <zj502407679@gmail.com>
2019-02-22 23:57:50 +0800

    sakura: update camera prop(still use O camera blobs)
    Change-Id: I86a45df50ab671ba1c2439738db56dd13c43362e

--- device.mk
+++ device.mk
@@ -170,18 +170,16 @@ PRODUCT_PACKAGES += \
     vendor.qti.hardware.camera.device@1.0_vendor
 
 PRODUCT_PROPERTY_OVERRIDES += \
-    persist.vendor.camera.display.umax=1920x1080
-    persist.vendor.camera.display.lmax=1280x720
-    vendor.camera.hal1.packagelist=com.skype.raider,com.google.android.talk \
-    vendor.camera.lowpower.record.enable=1 \
+    camera.display.lmax=1280x720 \
+    camera.display.umax=1920x1080 \
+    camera.lowpower.record.enable=1 \
     media.camera.ts.monotonic=1 \
     persist.camera.gyro.disable=0 \
     persist.camera.isp.clock.optmz=0 \
     persist.camera.stats.test=5 \
     persist.vendor.qti.telephony.vt_cam_interface=1 \
     vidc.enc.dcvs.extra-buff-count=2 \
-    persist.camera.HAL3.enabled=1 \
-    vendor.camera.aux.packagelist=org.codeaurora.snapcam,com.android.camera,org.mokee.snap
+    persist.camera.HAL3.enabled=1
 
 PRODUCT_PACKAGES += \
     Snap
