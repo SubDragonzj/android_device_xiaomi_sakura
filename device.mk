6fa6366e05725e5afe1932fab593289e14b9efbe
subdragonzj <zj502407679@gmail.com>
2019-03-11 22:17:45 +0800

    Revert "sakura: Add dummy perf profile."
    This reverts commit 668b05e94b12aebc790487ed7bba87c9fd653a52.

--- device.mk
+++ device.mk
@@ -309,8 +309,7 @@ PRODUCT_COPY_FILES += \
 
 # Powerhint
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/powerhint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.xml \
-    $(LOCAL_PATH)/configs/perf/perf-profile0.conf:$(TARGET_COPY_OUT_VENDOR)/etc/perf/perf-profile0.conf
+    $(LOCAL_PATH)/configs/powerhint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.xml
     
 # QTI App Permissions
 PRODUCT_COPY_FILES += \
