319f31a1ec9bec48a1b9c4b79e95609eceb0f425
subdragonzj <zj502407679@gmail.com>
2019-03-11 20:46:26 +0800

    sakura: update camera prop for Pie
    Change-Id: I5c90a7207cbbfde87cb4057a045076d5c18443b2

--- prop.mk
+++ prop.mk
@@ -58,17 +58,18 @@ ro.qualcomm.bt.hci_transport=smd
 
 # Camera
 PRODUCT_PROPERTY_OVERRIDES += \
-camera.display.lmax=1280x720 \
-camera.display.umax=1920x1080 \
-camera.lowpower.record.enable=1 \
+persist.vendor.camera.display.umax=1920x1080 \
+persist.vendor.camera.display.lmax=1280x720 \
+vendor.camera.lowpower.record.enable=1 \
 media.camera.ts.monotonic=1 \
 persist.camera.gyro.disable=0 \
 persist.camera.isp.clock.optmz=0 \
 persist.camera.stats.test=5 \
 persist.vendor.qti.telephony.vt_cam_interface=1 \
 vidc.enc.dcvs.extra-buff-count=2 \
-persist.camera.HAL3.enabled=1
-vendor.camera.aux.packagelist=org.codeaurora.snapcam,com.android.camera,org.mokee.snap
+persist.camera.HAL3.enabled=1 \
+vendor.camera.aux.packagelist=org.codeaurora.snapcam,com.android.camera,org.mokee.snap \
+vendor.camera.hal1.packagelist=com.skype.raider,com.google.android.talk
 
 # Cne
 PRODUCT_PROPERTY_OVERRIDES += \
