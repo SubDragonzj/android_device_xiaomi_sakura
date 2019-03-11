274b012c0c612800a5d2b2369fb5541786e0602f
subdragonzj <zj502407679@gmail.com>
2019-03-11 22:08:16 +0800

    sakura: device.mk: use prebuilt camera hidl HAL
    Change-Id: Id6e0412e73fad722effa021e3fa6259790da1c85

--- device.mk
+++ device.mk
@@ -124,12 +124,6 @@ PRODUCT_COPY_FILES += \
 
 # Camera
 PRODUCT_PACKAGES += \
-    android.hardware.camera.provider@2.4-impl \
-    android.hardware.camera.provider@2.4-service \
-    vendor.qti.hardware.camera.device@1.0 \
-    vendor.qti.hardware.camera.device@1.0_vendor
-
-PRODUCT_PACKAGES += \
     Snap
 
 # Configstore
