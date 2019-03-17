f4a84a186c09a573d2b4c62ce5262fdace4e1da6
subdragonzj <zj502407679@gmail.com>
2019-03-17 19:09:06 +0800

    sakura: remove TARGET_PLATFORM_DEVICE_BASE
    Change-Id: Ia4f51c3814a0b8794c7570951f69ec134ae2884d

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -195,7 +195,6 @@ USE_OPENGL_RENDERER := true
 
 # Init
 TARGET_INIT_VENDOR_LIB := libinit_sakura
-TARGET_PLATFORM_DEVICE_BASE := /devices/soc/
 TARGET_RECOVERY_DEVICE_MODULES := libinit_sakura
 
 # Keystore
