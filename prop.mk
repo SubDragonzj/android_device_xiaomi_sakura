9cea1240b4bde94253bd27c13d13f985b0ff5138
subdragonzj <zj502407679@gmail.com>
2019-03-18 23:37:52 +0800

    Revert "sakura: update camera prop for Pie"
    This reverts commit 319f31a1ec9bec48a1b9c4b79e95609eceb0f425.

--- prop.mk
+++ prop.mk
@@ -56,18 +56,17 @@ ro.qualcomm.bt.hci_transport=smd
 
 # Camera
 PRODUCT_PROPERTY_OVERRIDES += \
-persist.vendor.camera.display.umax=1920x1080 \
-persist.vendor.camera.display.lmax=1280x720 \
-vendor.camera.lowpower.record.enable=1 \
+camera.display.lmax=1280x720 \
+camera.display.umax=1920x1080 \
+camera.lowpower.record.enable=1 \
 media.camera.ts.monotonic=1 \
 persist.camera.gyro.disable=0 \
 persist.camera.isp.clock.optmz=0 \
 persist.camera.stats.test=5 \
 persist.vendor.qti.telephony.vt_cam_interface=1 \
 vidc.enc.dcvs.extra-buff-count=2 \
-persist.camera.HAL3.enabled=1 \
-vendor.camera.aux.packagelist=org.codeaurora.snapcam,com.android.camera,org.mokee.snap \
-vendor.camera.hal1.packagelist=com.skype.raider,com.google.android.talk
+persist.camera.HAL3.enabled=1
+vendor.camera.aux.packagelist=org.codeaurora.snapcam,com.android.camera,org.mokee.snap
 
 # Cne
 PRODUCT_PROPERTY_OVERRIDES += \
