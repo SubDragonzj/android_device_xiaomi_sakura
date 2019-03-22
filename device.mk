09d3de50eb13173dd8ef53a6dcd765b26f5dfa8f
subdragonzj <zj502407679@gmail.com>
2019-03-22 22:42:16 +0800

    sakura: oss: allow build OSS camera HAL
    Change-Id: I3f6f7eae1a9775df39221463dcdd2dc704da8e39

--- device.mk
+++ device.mk
@@ -125,7 +125,12 @@ PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service \
     vendor.qti.hardware.camera.device@1.0 \
-    vendor.qti.hardware.camera.device@1.0_vendor
+    vendor.qti.hardware.camera.device@1.0_vendor \
+    camera.device@3.2-impl
+
+PRODUCT_PACKAGES += \
+    camera.msm8953 \
+    libmm-qcamera
 
 PRODUCT_PACKAGES += \
     Snap
