PixelExperience-Devices/device_xiaomi_sakura

sakura: Update Widevine from LA.UM.8.6.2.r1-03700-89xx.0
Change-Id: I94e4f0a181c1c8ae1e21c026b35b7b87af52b61f

device.mk
@@ -165,7 +165,8 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.drm@1.0-impl \
     android.hardware.drm@1.0-service \
-    android.hardware.drm@1.2-service.clearkey
+    android.hardware.drm@1.2-service.clearkey \
+    android.hardware.drm@1.2-service.widevine
 
 # Ebtables
 PRODUCT_PACKAGES += \

SubDragonzj/android_device_xiaomi_sakura

device.mk
@@ -164,7 +164,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.drm@1.0-impl \
     android.hardware.drm@1.0-service \
-    android.hardware.drm@1.1-service.clearkey
+    android.hardware.drm@1.2-service.clearkey
 
 # Ebtables
 PRODUCT_PACKAGES += \